67641e703e535d29cc4163d57cb63c61c6d5045f
sfallmann <jorundr@hotmail.com>
2018-08-06 01:18:11 -0400

    Add tests for cache
    Testing set, get, checkAndSet
    Flushing and quitting the connection to Memcachier server after tests complete

--- server/cache/mem.js
+++ server/cache/mem.js
@@ -1,13 +1,19 @@
 'use strict';
-require('../config/config');
-
 const memjs = require('memjs');
 const client = memjs.Client.create();
 const axios = require('axios');
+const isURL = require('validator/lib/isURL');
 
 const cache = {
   set: (key, value) => {
     return new Promise((resolve, reject) => {
+
+      if (value !== Object(value)) {
+        const error = new Error('Cache values must be objects');
+        reject(error);
+        return;
+      }
+
       const buff = Buffer.from(JSON.stringify(value));
       client.set(key, buff, {expires: 60 * 60}, (err, val) => {
         if (err) {
@@ -31,6 +37,9 @@ const cache = {
     })  
   },
   checkAndSet: async (key) => {
+
+    if (!isURL(key)) throw new Error(`Key value "${key}" was not a valid URL`);
+
     let value = await cache.get(key);
 
     if (!value) {
@@ -40,7 +49,10 @@ const cache = {
     } else {
       return value;
     }
-  }  
+  },
+  flush: () => client.flush(),
+  quit: () => client.quit()
+
 }
 
 module.exports = { cache };
