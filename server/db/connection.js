af81c7a9a1a4782ab41a6af133df4d36002dd160
sfallmann <jorundr@hotmail.com>
2018-08-08 12:23:49 -0400

    Retry db connection 3  times before exiting process

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,5 +1,33 @@
 'use strict';
 const mongoose = require('mongoose');
+const logger = require('../logger');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+
+async function tryToConnect(retry) {
+  return new Promise((resolve, reject) => {
+    setTimeout(async() => {
+      mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+        .then(() => resolve())
+        .catch(reject);
+    }, retry * 1000);
+  })
+
+}
+
+async function connectToDb() {
+
+  for (let i = 0; i < 3; i++) {
+    try {
+      await tryToConnect(i);
+      return;
+    }
+    catch(e) {
+      logger.info(`Attempt #${i + 1} to connect to db failed`);
+      logger.error(e.message);
+    }
+  }
+  process.exit(1);
+}
+
+connectToDb();
